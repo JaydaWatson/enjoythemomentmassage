884b4be136f8d29e65ab89350dc33fc0eaff8a95
JaydaWatson <60038607+JaydaWatson@users.noreply.github.com>
2022-07-20 14:46:47 -0400

    fixing errors

--- App.test.js
+++ App.test.js
@@ -60,8 +60,7 @@ if (process.env.NODE_ENV === 'production'){
   app.use(express.static('Enjoy-Frontend/build'));
 }
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.port || 3000, () => {// port = this.address().port; 
+  console.log(listening in ${app.settings.env} mode);});
 
 // app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) })
