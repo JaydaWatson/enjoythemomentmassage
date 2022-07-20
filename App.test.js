68206741445833ec3fa14c55ef73f040c33b6038
JaydaWatson <60038607+JaydaWatson@users.noreply.github.com>
2022-07-20 15:09:32 -0400

    fixing

--- App.test.js
+++ App.test.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const bodyparser = require('body-parser');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 require('./Schema/index');
 
