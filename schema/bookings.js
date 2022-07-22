ee9b5d45d955d48c49d7cad8163cc0b27f8e7a4a
JaydaWatson <60038607+JaydaWatson@users.noreply.github.com>
2022-07-22 15:22:11 -0400

    delete duplicates

--- schema/bookings.js
+++ schema/bookings.js
@@ -6,7 +6,7 @@ const BookingSchema = new Schema({
     'town': String,
     'state': String,
     'zip': String,
-    'startDate': String,
+    'startDate': {type:String, unique: true},
     'level': String,
     'duration': String,
     'vip': [{ type: String }]
